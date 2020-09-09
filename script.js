9acad15440fe20cf91db69e2f49bc791d87def24
yan <redchilik@gmail.com>
2020-09-09 23:02:30 +0300

    upd

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ new Vue({
         currentTab: 0,
         intervalTime: 5000,
         interval: '',
+        scrollFromTop: 0,
         tabs: [
             { title: 'Одностраничный сайт', icon: 'file' },
             { title: 'Лендинг', icon: 'file-alt' },
@@ -11,17 +12,21 @@ new Vue({
             { title: 'Магазин', icon: 'shopping-bag' },
         ]
     },
-    methods:{
-        startInterval(){
+    methods: {
+        startInterval() {
             clearInterval(this.interval)
             this.interval = setInterval(() => {
                 if (this.currentTab >= this.tabs.length - 1) this.currentTab = 0;
                 else this.currentTab++
             }, this.intervalTime)
+        },
+        scrollHandler(e) {
+            this.scrollFromTop = window.scrollY
         }
     },
     created() {
-        this.startInterval()
+        this.startInterval();
+        window.addEventListener('scroll', this.scrollHandler);
     },
     watch: {
         currentTab() {
