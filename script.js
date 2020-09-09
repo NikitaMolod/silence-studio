27c704da52cfb7bd7ba830848c6dbe742978a3a9
yan <redchilik@gmail.com>
2020-09-10 02:15:36 +0300

    mobile

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ new Vue({
         intervalTime: 5000,
         interval: '',
         scrollFromTop: 0,
+        shopCount:0,
         tabs: [
             { title: 'Одностраничный сайт', icon: 'file' },
             { title: 'Лендинг', icon: 'file-alt' },
@@ -22,11 +23,24 @@ new Vue({
         },
         scrollHandler(e) {
             this.scrollFromTop = window.scrollY
+        },
+        add(site){
+            let sites = localStorage.getItem('sites');
+            if(!sites)sites={};
+            else sites = JSON.parse(sites)
+            if(site)sites[site]=true;
+            localStorage.setItem('sites',JSON.stringify(sites));
+            this.shopCount = Object.keys(sites).map(key=>sites[key]).length;
+        },
+        remove(){
+            localStorage.removeItem('sites')
+            this.add()
         }
     },
     created() {
         this.startInterval();
         window.addEventListener('scroll', this.scrollHandler);
+        this.add();
     },
     watch: {
         currentTab() {
